7cb38f295404fb8ffa41f3b01f0d5780da56dc7a
Brendan  Cuddihy <bpcuddihy@gmail.com>
2019-03-24 08:45:43 -0700

    Return applicaiton/json in response

--- api/perp/app.py
+++ api/perp/app.py
@@ -1,7 +1,6 @@
-import json
 import logging
 
-from flask import Flask
+from flask import Flask, jsonify, request
 
 from perp.client import get_mysql_client
 from perp.excpetion import PerpException
@@ -10,19 +9,23 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-@app.route("/api/v1/table/<table_name>")
+@app.route("/api/v1/table/<table_name>", methods=["GET"])
 def select_all_from_table(table_name):
     client = get_mysql_client()
     try:
-        result = client.select_all_from_table(table_name)
-        return json.dumps(result)
+        result = client.select_all_from_table(
+            table_name,
+            request.args.get("order"),
+            request.args.get("num_rows")
+        )
+        return jsonify(result)
     except PerpException as e:
-        return str(e), 400
+        return jsonify(str(e)), 400
 
 
 @app.route("/api/v1/hello")
 def hello_world():
-    return "hello world !"
+    return jsonify("hello world !")
 
 
 if __name__ == '__main__':
